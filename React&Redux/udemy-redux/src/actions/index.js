2f28ba5a8fa3a368a386e4ecfd3d7578fe996142
chanha22 <sgu960401@sogang.ac.kr>
2017-11-17 03:12:53 +0900

    final

--- React&Redux/udemy-redux/src/actions/index.js
+++ React&Redux/udemy-redux/src/actions/index.js
@@ -1,6 +1,6 @@
-export function selectBook(book){
-  // selectBook is an ActionCreator, it needs to return an action
-  // an Object with a type property
+export function selectBook(book) {
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property.
   return {
     type: "BOOK_SELECTED",
     payload: book
